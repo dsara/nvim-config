97891c7bcd864c197efbfb5ffe980f333a3cbfd3
Dan Sara <tech@bitoperator.io>
2025-03-16 13:38:50 -0400

    add in neovim completions

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -16,12 +16,24 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
 			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
-			lspconfig.yamlls.setup({})
-			lspconfig.ts_ls.setup({})
-			lspconfig.bashls.setup({})
-			lspconfig.jsonls.setup({})
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.yamlls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.ts_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.bashls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.jsonls.setup({
+				capabilities = capabilities,
+			})
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gD", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
