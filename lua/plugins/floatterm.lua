77eb6ecdabf54ee3f69fec70c9c05016a871c366
Dan Sara <tech@bitoperator.io>
2025-03-23 21:17:02 -0400

    add in toggle command for terminal mode for floatterm

--- lua/plugins/floatterm.lua
+++ lua/plugins/floatterm.lua
@@ -2,5 +2,6 @@ return {
 	"voldikss/vim-floaterm",
 	config = function()
 		vim.keymap.set("n", "<leader>`", ":FloatermToggle<CR>", {})
+		vim.keymap.set("t", "<leader>``", "<C-\\><C-n>:FloatermToggle<CR>", {})
 	end,
 }
