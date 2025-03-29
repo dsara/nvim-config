026d0db2e84aa2c6b235be0876bb2161ca581a5a
Dan Sara <tech@bitoperator.io>
2025-03-29 18:12:48 -0400

    add in gitsigns hunk hotkeys

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,6 +1,9 @@
 return {
 	"lewis6991/gitsigns.nvim",
 	config = function()
-		require("gitsigns").setup({})
+		require("gitsigns").setup({
+			vim.keymap.set("n", "<leader>hr", ":Gitsigns reset_hunk<CR>", { noremap = true, silent = true }),
+			vim.keymap.set("n", "<leader>hp", ":Gitsigns preview_hunk_inline<CR>", { noremap = true, silent = true }),
+		})
 	end,
 }
