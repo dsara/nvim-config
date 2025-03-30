d67f218396b4a4c82f501d2c327cd25d396a253f
Dan Sara <tech@bitoperator.io>
2025-03-29 23:46:04 -0400

    make hunk commands more complex

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -2,8 +2,8 @@ return {
 	"lewis6991/gitsigns.nvim",
 	config = function()
 		require("gitsigns").setup({
-			vim.keymap.set("n", "<leader>hr", ":Gitsigns reset_hunk<CR>", { noremap = true, silent = true }),
-			vim.keymap.set("n", "<leader>hp", ":Gitsigns preview_hunk_inline<CR>", { noremap = true, silent = true }),
+			vim.keymap.set("n", "<leader>ghr", ":Gitsigns reset_hunk<CR>", { noremap = true, silent = true }),
+			vim.keymap.set("n", "<leader>ghp", ":Gitsigns preview_hunk_inline<CR>", { noremap = true, silent = true }),
 			vim.keymap.set("n", "<A-[>", ":Gitsigns prev_hunk<CR>", { noremap = true, silent = true }),
 			vim.keymap.set("n", "<A-]>", ":Gitsigns next_hunk<CR>", { noremap = true, silent = true }),
 		})
