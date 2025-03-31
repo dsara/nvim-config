8f059d740185b8eb2b305bb4a703d01b244a711d
Dan Sara <tech@bitoperator.io>
2025-03-30 20:00:56 -0400

    disable change notifications

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -32,4 +32,6 @@ require("lazy").setup({
 	install = { colorscheme = { "habamax" } },
 	-- automatically check for plugin updates
 	checker = { enabled = true },
+	-- disable change detection
+	change_detection = { notify = false },
 })
