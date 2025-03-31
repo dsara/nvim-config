36a0a3b2889fc33e57de83369e31e97de6f66b8a
Dan Sara <tech@bitoperator.io>
2025-03-30 22:34:44 -0400

    switch over to blink pairs

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,5 +1,16 @@
 return {
-	"windwp/nvim-autopairs",
-	event = "InsertEnter",
-	config = true,
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+		enabled = false,
+	},
+	{
+		"saghen/blink.pairs",
+		version = "*",
+		dependencies = "saghen/blink.download",
+		--- @module "blink.pairs"
+		--- @type blink.pairs.Config
+		opts = {},
+	},
 }
