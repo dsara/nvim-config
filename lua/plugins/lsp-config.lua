93acc841e832d3982b7f8c52ed170ebc60d817f3
Dan Sara <tech@bitoperator.io>
2025-03-30 21:23:47 -0400

    switch to blink instead of existing cmp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -15,25 +15,42 @@ return {
 	},
 	{
 		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
+		dependencies = { "saghen/blink.cmp" },
+		opts = {
+			servers = {
+				lua_ls = {},
+				yamlls = {},
+				ts_ls = {},
+				bashls = {},
+				jsonls = {},
+			},
+		},
+		config = function(_, opts)
 			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.yamlls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.ts_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.bashls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.jsonls.setup({
-				capabilities = capabilities,
-			})
+			for server, config in pairs(opts.servers) do
+				config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
+				lspconfig[server].setup(config)
+			end
+
+			-- cmp_nvim_lsp
+			-- local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			--
+			-- local lspconfig = require("lspconfig")
+			-- lspconfig.lua_ls.setup({
+			-- 	capabilities = capabilities,
+			-- })
+			-- lspconfig.yamlls.setup({
+			-- 	capabilities = capabilities,
+			-- })
+			-- lspconfig.ts_ls.setup({
+			-- 	capabilities = capabilities,
+			-- })
+			-- lspconfig.bashls.setup({
+			-- 	capabilities = capabilities,
+			-- })
+			-- lspconfig.jsonls.setup({
+			-- 	capabilities = capabilities,
+			-- })
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Signature info (LSP)" })
 			vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, { desc = "Signature help (LSP)" })
 			vim.keymap.set("n", "gD", vim.lsp.buf.definition, { desc = "Goto definition (LSP)" })
