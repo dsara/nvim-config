d03610404684fd1afe1d0745e52291f9aa914a4d
Dan Sara <tech@bitoperator.io>
2025-03-31 18:48:00 -0400

    add in hotkeys for tabs

--- lua/plugins/tabbar.lua
+++ lua/plugins/tabbar.lua
@@ -6,6 +6,18 @@ return {
 	},
 	init = function()
 		vim.g.barbar_auto_setup = false
+
+		local map = vim.api.nvim_set_keymap
+		local opts = { noremap = true, silent = true }
+
+		map("n", "<A-,>", "<cmd>BufferPrevious<cr>", opts)
+		map("n", "<A-.>", "<cmd>BufferNext<cr>", opts)
+		map("n", "<A-<>", "<cmd>BufferMovePrevious<cr>", opts)
+		map("n", "<A->>", "<cmd>BufferMoveNext<cr>", opts)
+		map("n", "<A-p>", "<cmd>BufferPin<cr>", opts)
+		map("n", "<A-c>", "<cmd>BufferClose<cr>", opts)
+		map("n", "<C-p>", "<cmd>BufferPick<cr>", opts)
+		map("n", "<C-s-p>", "<cmd>BufferPickDelete<cr>", opts)
 	end,
 	opts = {},
 	version = "^1.0.0",
