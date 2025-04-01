9b0279b7d7694625eb7a32d4beb190c184996773
Dan Sara <tech@bitoperator.io>
2025-03-31 20:37:09 -0400

    add in notifications and update term capabilities

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,6 +3,7 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
+vim.opt.termguicolors = true
 -- Space leader key and remove default functionality
 vim.g.mapleader = " "
 vim.keymap.set("n", "<Space>", "<Nop>", { silent = true })
