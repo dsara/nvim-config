b2577d669190f0873d0fa9a3f8f6137619bcf85a
Dan Sara <tech@bitoperator.io>
2025-04-06 12:28:51 -0400

    add in mini sessions as disabled

--- lua/plugins/sessions.lua
+++ lua/plugins/sessions.lua
@@ -1,42 +1,55 @@
 return {
-	"stevearc/resession.nvim",
-	opts = {
-		extensions = {
-			barabar = {},
+	{
+		"stevearc/resession.nvim",
+		dependencies = {
+			"romgrk/barbar.nvim",
 		},
+		opts = {
+			extensions = {
+				barabar = {},
+			},
+		},
+		enabled = true,
+		config = function()
+			local resession = require("resession")
+			resession.setup()
+
+			vim.keymap.set(
+				"n",
+				"<leader>ss",
+				resession.save,
+				{ noremap = true, silent = true, desc = "Save session (resession)" }
+			)
+			vim.keymap.set(
+				"n",
+				"<leader>sl",
+				resession.load,
+				{ noremap = true, silent = true, desc = "Load session (resession)" }
+			)
+			vim.keymap.set(
+				"n",
+				"<leader>sd",
+				resession.delete,
+				{ noremap = true, silent = true, desc = "Delete session (resession)" }
+			)
+
+			vim.keymap.set("n", "<leader>sf", function()
+				require("resession").load(nil, { dir = "dirsession", reset = false })
+			end, { noremap = true, silent = true, desc = "Load folder session (ressession)" })
+
+			vim.api.nvim_create_autocmd("VimLeavePre", {
+				callback = function()
+					resession.save(vim.fn.getcwd(), { dir = "dirsession", notify = false })
+				end,
+			})
+		end,
 	},
-	enabled = true,
-	config = function()
-		local resession = require("resession")
-		resession.setup()
-
-		vim.keymap.set(
-			"n",
-			"<leader>ss",
-			resession.save,
-			{ noremap = true, silent = true, desc = "Save session (resession)" }
-		)
-		vim.keymap.set(
-			"n",
-			"<leader>sl",
-			resession.load,
-			{ noremap = true, silent = true, desc = "Load session (resession)" }
-		)
-		vim.keymap.set(
-			"n",
-			"<leader>sd",
-			resession.delete,
-			{ noremap = true, silent = true, desc = "Delete session (resession)" }
-		)
-
-		vim.keymap.set("n", "<leader>sf", function()
-			require("resession").load(nil, { dir = "dirsession", reset = false })
-		end, { noremap = true, silent = true, desc = "Load folder session (ressession)" })
-
-		vim.api.nvim_create_autocmd("VimLeavePre", {
-			callback = function()
-				resession.save(vim.fn.getcwd(), { dir = "dirsession", notify = false })
-			end,
-		})
-	end,
+	{
+		"echasnovski/mini.sessions",
+		version = "*",
+		enabled = false,
+		config = function()
+			require("mini.sessions").setup()
+		end,
+	},
 }
