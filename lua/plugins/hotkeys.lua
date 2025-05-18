be81306d1bb80776da4c5ce4c0774640ff3b0443
Dan Sara <tech@bitoperator.io>
2025-05-18 12:43:41 -0400

    add in hardtime plugin

--- lua/plugins/hotkeys.lua
+++ lua/plugins/hotkeys.lua
@@ -49,4 +49,11 @@ return {
       })
     end,
   },
+  {
+    'm4xshen/hardtime.nvim',
+    lazy = false,
+    dependencies = { 'MunifTanjim/nui.nvim' },
+    opts = {},
+    enabled = true,
+  },
 }
