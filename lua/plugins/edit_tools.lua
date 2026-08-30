4b32431e6b9e97d91d1c95f12d7cd408d033258d
Dan Sara <tech@bitoperator.io>
2026-08-29 22:45:19 -0400

    plugins: disable blink pairs

--- lua/plugins/edit_tools.lua
+++ lua/plugins/edit_tools.lua
@@ -17,9 +17,10 @@ return {
     'saghen/blink.pairs',
     version = '*',
     dependencies = 'saghen/blink.download',
+    build = function() require('blink.pairs').download():pwait(60000) end,
     --- @module "blink.pairs"
     --- @type blink.pairs.Config
     opts = {},
-    enabled = true,
+    enabled = false,
   },
 }
